09353602cd315a7dd195fdae2f0cea8cb0b84756
Daniel Patterson <dan24patt@gmail.com>
2017-01-29 21:13:58 +0000

    Seed development database
    To make testing the client easier, run:
    
      rails db:seed
    
    to add some data.

--- address-book-api/db/seeds.rb
+++ address-book-api/db/seeds.rb
@@ -6,3 +6,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+organisation_details = ContactDetails.create(
+  email:    'old_timers@example.com',
+  country:  'UK',
+  postcode: 'AB1 2CD'
+)
+
+organisation = Organisation.create name: 'Historical figures', contact_details: organisation_details
+
+[
+  'Genghis Khan',
+  'Sigmund Freud',
+  'Napoleon Bonaparte',
+  'Beethoven',
+  'Billy the Kid',
+  'Socrates',
+  'Abraham Lincoln',
+  'Joan of Arc'
+].each do |name|
+  details = ContactDetails.create(
+    email:          "#{name}@example.com",
+    phone_number:   '8675309',
+    street_address: '123 Fake Street',
+    country:        'UK',
+    postcode:       'AB1 2CD'
+  )
+
+  Person.create(name: name, contact_details: details)
+end
+
+organisation.people << Person.first(3)
